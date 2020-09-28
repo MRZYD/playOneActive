9974fd7622d6de54c74f3519af5c1b5be2da0494
MRZYD <2416389097@qq.com>
2020-09-28 16:07:39 +0800

    style:修改样式

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ function openPopup() {
 var userName = '';  //用户名
 var userTel = '';  //电话号码
 var formType = '';  //提交信息位置
+var requirement = '';  //其他信息
 
 function namePopupOnclick(e){
     userName = e;
@@ -24,38 +25,97 @@ function telPopupOnclick(e) {
     }
 }
 
+$(".thr-edu-type span").click(function (event) {
+    $(this).addClass("hover").siblings().removeClass("hover");
+})
+
 //提交表单
 function formBtn(index) {
     if(index == 1){
-        formType = 'Top';  //最上面 
+        formType = '页面顶部';  //最上面 
     }else if(index == 2){
-        formType = 'Bottom'; //最下面
+        formType = '页面底部'; //最下面
     }else{
-        formType = 'Popup'; //弹窗
+        formType = '页面弹窗'; //弹窗
     }
-    console.log(userName, userTel, formType);
-    $(".formName").val('');
-    $(".formTel").val('');
-    // addApply(document.applyForm);
-}
 
-function addApply() {
-    Requset({
-        data: checkVal({
-            mobile: '#mobile',
-            userName: '#userName'
-        }),
-        method: 'post',
-        success: function (res) {
-            alert("提交成功！");
-            $("#mobile,#userName").val("");
-            console.log('000');
-        },
-        fail: function (err) {
-            alert("提交成功！");
-            $("#mobile,#userName").val("");
-            console.log('111');
+    if(userName=="" || userName=="您的姓名"){
+        alert("请输入姓名!");
+        if(index == 1){
+            $("#userName1").focus();
+        }else if(index == 2){
+            $("#userName2").focus();
+        }else{
+            $("#userName3").focus();
         }
-    })
+        return;
+    }
 
+    var pattern = /^1{1}[0-9]{10}$/;
+    if(userTel=="" || userTel=="您的电话"){
+        alert("请输入手机号!");
+        if(index == 1){
+            $("#userTel1").focus();
+        }else if(index == 2){
+            $("#userTel2").focus();
+        }else{
+            $("#userTel3").focus();
+        }
+        return;
+    } else {
+        if (!pattern.test(userTel)) {
+            alert("请输入正确的手机号！");
+            if(index == 1){
+                $("#userTel1").focus();
+            }else if(index == 2){
+                $("#userTel2").focus();
+            }else{
+                $("#userTel3").focus();
+            }
+            return;
+        }
+    }	
+    var eduType = '';
+    $(".thr-edu-type span").each(function () {
+        if ($(this).hasClass('hover')) {
+            eduType = $(this).text();
+        }
+    });	
+    if (index == 1 && !eduType) {
+        alert("请选择培训类型！");
+        return;
+    }
+    requirement = formType + ((index !== 1 || !eduType) ? '' : (',' + eduType))     
+    addApply(index);
 }
+
+function addApply(index){
+    $.ajax({
+        url : "https://admin.96koo.net/moma/saveOpportunity",
+        data : {
+            compellation:userName,
+            phone:userTel,
+            referer: window.location.href,
+            industry: requirement
+        },
+        type : "post",
+        dataType : "json",
+        cache : false,
+        async : false,
+        success : function(res) {
+            if(res.msg == 'OK') {
+                alert("提交信息成功");
+                $(".formTel,.formName").val("");
+                $(".thr-edu-type span").each(function (event) {$(this).removeClass("hover");})
+                userName = '';
+                userTel = '';
+                requirement = '';
+            }
+        },
+        error : function(error) {
+            console.log("服务出现问题，请稍后再试！")
+        }
+    });
+}
+
+		
